c207a022300bb9978323422bae0ab70224e2a9f1
n.fouche <n.fouche@attineos.com>
2019-07-09 10:39:10 +0200

    Updates

---
+++ precache-manifest.3f6455a3cbddc9b01a5001d98ad551e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48d63b7530b67802f95354ba882d191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f106948998402ac7d777",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "f106948998402ac7d777",
+    "url": "/static/js/main.5bcb0cf7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
