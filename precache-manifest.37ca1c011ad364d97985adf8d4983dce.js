96a638d58f3d1d1725979808567c2ca66275cad0
n.fouche <n.fouche@attineos.com>
2019-07-09 11:31:50 +0200

    build

---
+++ precache-manifest.37ca1c011ad364d97985adf8d4983dce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ce3bd2af6a9a107c47df6725a021b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3956e73fbcac15c52cb3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f699c3f489e53d583270",
+    "url": "/static/js/2.287df5fe.chunk.js"
+  },
+  {
+    "revision": "3956e73fbcac15c52cb3",
+    "url": "/static/js/main.c0d89415.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
